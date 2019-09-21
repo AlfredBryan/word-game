8c1580de6525b63448f47f9566e818192aab6b8d
Lord Jerry <lovinjerry004@gmail.com>
2019-09-21 06:19:45 -0700

    implement database schema for users

---
+++ server/models/users.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const users = new Schema({
+
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    maxlength: 30,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    maxlength: 50,
+  },
+  
+  gender: {
+    type: String,
+    required: true,
+    unique: false,
+    maxlength: 6,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    unique: false,
+    maxlength: 70,
+  },
+
+  reg_date: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+
+  pic: {
+    type: String,
+    required: false,
+    unique: false,
+  }
+
+});
+
+module.exports = mongoose.model('Users', users);
